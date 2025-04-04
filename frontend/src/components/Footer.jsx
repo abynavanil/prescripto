9c3234ea6d0a7d1ef0c27e0527b9dfbd99c14504
abynavanil <abhinavanil8686@gmail.com>
2025-04-04 13:47:44 +0530

    first-commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='md:mx-10'>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10  mt-40 text-sm'>
+
+        <div>
+          <img className='mb-5 w-40' src={assets.logo} alt="" />
+          <p className='w-full md:w-2/3 text-gray-600 leading-6'>Prescripto simplifies the process of booking doctor appointments by providing a seamless and intuitive platform for patients. Users can browse doctors based on specialty, location, and availability, ensuring they find the right healthcare professional with ease. The app offers real-time appointment scheduling, instant confirmations, and notifications to keep users updated. With a responsive and secure design, Prescripto enhances the healthcare experience by reducing wait times and making medical consultations more accessible.</p>
+        </div>
+
+        <div>
+          <p className='text-xl font-medium mb-5'>COMPANY</p>
+          <ul className='flex flex-col gap-2 text-gray-600'>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+
+        <div>
+          <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+          <ul className='flex flex-col gap-2 text-gray-600'>
+            <li>+1-212-456-7890</li>
+            <li>Prescripto@gmail.com</li>
+          </ul>
+        </div>
+
+      </div>
+
+      <div>
+        <hr />
+        <p className='py-5 text-sm text-center'>Copyright 2024 @ Prescripto.com - All Right Reserved.</p>
+      </div>
+
+    </div>
+  )
+}
+
+export default Footer
